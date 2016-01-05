ba67176b2da3ed5013f497b03f29ca6ec067b49c
Steven Rowney <stevenrowney@stevens-macbook-pro.local>
2016-01-05 18:41:16 +0000

    Initial Commit

---
+++ GoodAsOldPhones/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoodAsOldPhones
+//
+//  Created by Steven Rowney on 05/01/2016.
+//  Copyright © 2016 The Blue Cabrio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
